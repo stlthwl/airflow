cd040a9ec536e8d95c9e6ee1e14e8a442adaee62
stlthwl <stlthwl92@mail.ru>
2025-06-01 02:01:21 +0300

    test

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.providers.http.sensors.http import HttpSensor
 from datetime import datetime, timedelta
 import json
 
@@ -14,12 +13,6 @@ default_args = {
     'catchup': False,
 }
 
-def check_response(response):
-    from airflow.exceptions import AirflowException
-    if response.status_code != 200:
-        raise AirflowException(f"Request failed with code {response.status_code}: {response.text}")
-    return True
-
 with DAG(
     'test_dag',
     default_args=default_args,
@@ -28,21 +21,9 @@ with DAG(
     tags=['http', 'telegram'],
 ) as dag:
 
-    check_endpoint_available = HttpSensor(
-        task_id='check_endpoint_available',
-        http_conn_id=None,
-        endpoint='https://90.156.154.231:3001/api/v1/sendMessageTelegram',
-        method='POST',
-        request_params={},
-        response_check=lambda response: True,
-        extra_options={"verify": False},
-        poke_interval=5,
-        timeout=20
-    )
-
     send_telegram_message = SimpleHttpOperator(
         task_id='send_telegram_message',
-        http_conn_id=None,
+        http_conn_id=None,  # Можно создать HTTP connection в Airflow для этого URL
         endpoint='https://90.156.154.231:3001/api/v1/sendMessageTelegram',
         method='POST',
         headers={
@@ -54,56 +35,10 @@ with DAG(
             "textMessage": "test airflow"
         }),
         extra_options={
-            "verify": False
+            "verify": False  # Отключаем проверку SSL (не рекомендуется для production)
         },
-        response_check=check_response,
         log_response=True,
         retries=0,
     )
 
-    check_endpoint_available >> test_dag
-
-# from airflow import DAG
-# from airflow.providers.http.operators.http import SimpleHttpOperator
-# from datetime import datetime, timedelta
-# import json
-#
-# default_args = {
-#     'owner': 'admin',
-#     'start_date': datetime(2024, 5, 1),
-#     'retries': 0,
-#     'retry_delay': timedelta(minutes=0),
-#     'max_retry_delay': timedelta(minutes=0),
-#     'depends_on_past': False,
-#     'catchup': False,
-# }
-#
-# with DAG(
-#     'test_dag',
-#     default_args=default_args,
-#     schedule_interval='*/15 * * * *',
-#     catchup=False,
-#     tags=['http', 'telegram'],
-# ) as dag:
-#
-#     send_telegram_message = SimpleHttpOperator(
-#         task_id='send_telegram_message',
-#         http_conn_id=None,  # Можно создать HTTP connection в Airflow для этого URL
-#         endpoint='https://90.156.154.231:3001/api/v1/sendMessageTelegram',
-#         method='POST',
-#         headers={
-#             "Content-Type": "application/json",
-#             "Accept": "application/json"
-#         },
-#         data=json.dumps({
-#             "userTechId": 1080079179,
-#             "textMessage": "test airflow"
-#         }),
-#         extra_options={
-#             "verify": False  # Отключаем проверку SSL (не рекомендуется для production)
-#         },
-#         log_response=True,
-#         retries=0,
-#     )
-#
-#     send_telegram_message
+    send_telegram_message
